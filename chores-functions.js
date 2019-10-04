bb449ed5b1af2371354a6dfbb8be9b78689f2997
Dj <ritchey0713@gmail.com>
2019-10-04 15:41:35 -0400

    add file

---
+++ chores-functions.js
@@ -0,0 +1,38 @@
+const getChores = () => {
+    const choresJSON = localStorage.getItem("chores")
+
+    if (choresJSON != null) {
+        return JSON.parse(choresJSON)
+    } else {
+        return []
+    }
+
+}
+
+// generate chore dom
+const generateChores = (chore) => {
+    const choreEl = document.createElement("p")
+        if (chore.title.length > 0){
+            choreEl.textContent = chore.title 
+        } else {
+            choreEl.textContent = "Unnamed Chore"
+        }
+        return choreEl;
+}
+
+const renderList = (chores, filters) => {
+    const filteredList = chores.filter((note)=> {
+        return note.title.toLowerCase().includes(filters.searchTerm.toLowerCase())
+    })
+    document.querySelector("#chores").innerHTML = ""
+    filteredList.forEach((chore)=> {
+        const choreEl = generateChores(chore)
+        document.querySelector("#chores").appendChild(choreEl)
+    })
+
+};
+
+const saveChore = (chores) => {
+    localStorage.setItem("chores", JSON.stringify(chores))
+}
+
